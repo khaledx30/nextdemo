5aa88ed3b45d52685efea86371cba89ec7132eac
khaled essam <khaledx30@gmail.com>
2023-07-19 10:22:13 +0300

    adding configs

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,18 @@
 import styles from "./page.module.css";
+import Image from "next/image";
 
 export default function About() {
-  return <div className={styles.container}>About</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          // src="/public/k.bmp"
+          src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          fill={true}
+          className={styles.img}
+        />
+      </div>
+      <div className={styles.textContainer}></div>
+    </div>
+  );
 }
