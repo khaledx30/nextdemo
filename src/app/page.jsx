8a5b111ad7560858411f8f839ae21f7ee24d5900
khaled essam <khaledx30@gmail.com>
2023-07-19 07:55:37 +0300

    adding stuff

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,8 +3,18 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className={styles.div}>
-      <div>
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          Better design for your digital products.
+        </h1>
+        <p className={styles.desc}>
+          Turning your Idea into reality we bring together the team for the
+          global tech industry
+        </p>
+        <button className={styles.button}>see our works</button>
+      </div>
+      <div className={`${styles.item} ${styles.fex}`}>
         <Image
           src="/hero.png"
           width={500}
