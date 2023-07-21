4ece768fd39681fb9fe3963debfbc30cdc0b1724
khaled essam <khaledx30@gmail.com>
2023-07-21 07:18:18 +0300

    s

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Image from "next/image";
 import Button from "@/components/Button/Button";
 
 export default function About() {
