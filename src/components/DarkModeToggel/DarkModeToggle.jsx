15aa3ea993986b9a2d3a0f104b3712ffb551fdc5
khaled essam <khaledx30@gmail.com>
2023-07-27 04:32:54 +0300

    ds

--- src/components/DarkModeToggel/DarkModeToggle.jsx
+++ src/components/DarkModeToggel/DarkModeToggle.jsx
@@ -1,16 +1,23 @@
 "use client";
+import { useContext } from "react";
 import styles from "./darkmode.module.css";
+import { ThemeContext } from "@/context/ThemeContext";
 
 // import {ThemeContext} from
 let mode = "dark";
 export default function DarkModeToggle() {
+  const { toggle, mode } = useContext(ThemeContext);
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onClick={toggle}>
       <div className={styles.icon}>🌙</div>
       <div className={styles.icon}>🔆</div>
       <div
         className={styles.ball}
-        style={mode === "light" ? { left: "2px" } : { right: "2px" }}
+        style={
+          mode === "light"
+            ? { transform: "translateX(20px)" }
+            : { transform: "translateX(0px)" }
+        }
       />
     </div>
   );
